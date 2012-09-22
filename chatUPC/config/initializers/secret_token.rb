56747ee1ae73b2dc01051fa17694d450f534fd32
Daniel <daalsuol@gmail.com>
2012-09-22 11:55:18 -0500

    chatUPC

---
+++ chatUPC/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChatUPC::Application.config.secret_token = '546fd79eefc9e276596414d1968c8a0a52343f81d26cd153731bc3ed0920dc90edccee6e0c98601c8189dc2d0c5614efeff2fcef0bcf624eb4570d3866a5ccc0'
