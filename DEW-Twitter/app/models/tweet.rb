6748324be657eac2c2d5770f88493d49a21fa824
Daniel <daalsuol@gmail.com>
2012-09-29 13:34:30 -0500

    final twitter

--- DEW-Twitter/app/models/tweet.rb
+++ DEW-Twitter/app/models/tweet.rb
@@ -1,15 +1,20 @@
 class Tweet < ActiveRecord::Base
 
 Twitter.configure do |config|
-  config.consumer_key = '8ohKDAxLPzycMrJ8myT8mQ' 
-  config.consumer_secret = 'Tptace33ZNTnTcLx1QQXwkeXkS9gWV82w6UMBA51Y'
-  config.oauth_token = '313482608-SvCgWS869NKqbyQnr04t1uC1V6AdfgpmSOvskmA'
-  config.oauth_token_secret = 'tQBycAI7p134gI1IOueEcgcMQrPHWgCm6H8GuQZmXFg'
+  config.consumer_key = 'pToks66RC32cevL0QOdKBw' 
+  config.consumer_secret = 'TvS5TwBprhBaoNrH4loUNnLSnHEivE1xUZMa9Oebe4'
+  config.oauth_token = '313482608-4uofrTHoKIv9ebIs5aHX5ZCntDDaQMqkOB5LIQT1'
+  config.oauth_token_secret = 'n3xvumJVRKr7p8HxRaWvbJcpQRwPrVmudGg2MQA3NA'
 end
 
   def read_message 
   	return "sin user" if self.user.blank?
   	Twitter.user_timeline(self.user).first.text
+  end
+
+  def update_status
+  	return "sin update" if self.message.blank?
+  	Twitter.update(self.message)
   end
 
 end
--- DEW-Twitter/config/routes.rb
+++ DEW-Twitter/config/routes.rb
@@ -1,6 +1,7 @@
 DEWTwitter::Application.routes.draw do
   resources :tweets
   match "user" => "tweets#read_message"
+  match "update" => "tweets#update_status"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
